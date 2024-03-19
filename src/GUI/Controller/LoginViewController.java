835c383ca33e29a0b074989a46c99836c0c30980
JowanaA <146068025+JowanaA@users.noreply.github.com>
2024-03-19 22:34:29 +0100

    Ændringer på Fxml + start af MainView.fxml

--- src/GUI/Controller/LoginViewController.java
+++ src/GUI/Controller/LoginViewController.java
@@ -1,5 +1,18 @@
 package GUI.Controller;
 
-public class LoginViewController {
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginViewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
     //primaryStage.setResizable(false);
 }
--- src/GUI/Controller/MainViewController.java
+++ src/GUI/Controller/MainViewController.java
@@ -1,4 +1,19 @@
 package GUI.Controller;
 
-public class MainViewController {
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
 }
