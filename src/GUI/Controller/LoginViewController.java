cbc884f0876315702882157bf932ba8dd7b8b504
JowanaA <146068025+JowanaA@users.noreply.github.com>
2024-03-19 23:30:50 +0100

    MenuItem + show LoginView

--- src/GUI/Controller/LoginViewController.java
+++ src/GUI/Controller/LoginViewController.java
@@ -1,18 +1,31 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginViewController implements Initializable {
+    public Button cancelBtn;
+    public Button logInBtn;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    public void cancelBtnHandle(ActionEvent actionEvent) {
+    }
+
+    public void loginBtnHandle(ActionEvent actionEvent) {
+    }
+
+
     //primaryStage.setResizable(false);
 }
--- src/GUI/Controller/MainViewController.java
+++ src/GUI/Controller/MainViewController.java
@@ -1,9 +1,13 @@
 package GUI.Controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +15,31 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
+    public MenuItem menuAuthorisedLogin;
+    public MenuItem menuHome;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
 
+    }
+
+    public void handleAuthorisedLogin(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/loginView.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Login");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Could not load loginView.fxml");
+            alert.showAndWait();
+        }
+    }
+
+    public void handleHome(ActionEvent actionEvent) {
     }
 }
