2e5ebcb8ae37b0293b8ebbdbacabdeac31fd0791
nikolaj1307 <nikolaj1307@live.dk>
2024-04-02 21:20:26 +0200

    Email regex update

--- src/GUI/Controller/CreateUserController.java
+++ src/GUI/Controller/CreateUserController.java
@@ -104,7 +104,7 @@ public class CreateUserController implements Initializable {
 
     // Method to validate email format using regex
     private boolean isValidEmail(String email) {
-        String regex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
         return email.matches(regex);
     }
 
