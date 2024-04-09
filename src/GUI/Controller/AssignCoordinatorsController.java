f4cef15c5b742158af35c3311ae00e37c429954a
nikolaj1307 <nikolaj1307@live.dk>
2024-04-09 13:39:14 +0200

    Assign coordinator fxml + controller

---
+++ src/GUI/Controller/AssignCoordinatorsController.java
@@ -0,0 +1,22 @@
+package GUI.Controller;
+
+import io.github.palexdev.materialfx.controls.MFXListView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AssignCoordinatorsController {
+
+    @FXML
+    private MFXListView<?> listView;
+
+    @FXML
+    void onClickBtnCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickBtnSave(ActionEvent event) {
+
+    }
+
+}
