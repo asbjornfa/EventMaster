981efd3b469ca9a4f274fdca4569ecf43754285d
nikolaj1307 <nikolaj1307@live.dk>
2024-04-11 12:45:26 +0200

    New AddReservationView + controller

---
+++ src/GUI/Controller/AddReservationViewController.java
@@ -0,0 +1,34 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+public class AddReservationViewController {
+
+    @FXML
+    private MenuButton ticketTypeDropDown;
+
+    @FXML
+    private TextField txtFieldCostumerEmail;
+
+    @FXML
+    private TextField txtFieldQuantity;
+
+    @FXML
+    public void onClickCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onClickSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onClickTicketTypeDropDown(ActionEvent event) {
+
+    }
+
+}
