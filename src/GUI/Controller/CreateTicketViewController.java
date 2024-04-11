05a30fd6592915308eb08e384b870313d111b8e2
nikolaj1307 <nikolaj1307@live.dk>
2024-04-11 12:41:09 +0200

    New CreateTicketView

---
+++ src/GUI/Controller/CreateTicketViewController.java
@@ -0,0 +1,34 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+public class CreateTicketViewController {
+
+    @FXML
+    private MenuButton ticketTypeDropDown;
+
+    @FXML
+    private TextField txtFieldPrice;
+
+    @FXML
+    private TextField txtFieldQuantity;
+
+    @FXML
+    public void onClickCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onClickSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onClickTicketTypeDropDown(ActionEvent event) {
+
+    }
+
+}
