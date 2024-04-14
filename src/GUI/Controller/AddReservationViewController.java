05503fb171c5776a9af0e48138e8300724f92e2b
nikolaj1307 <nikolaj1307@live.dk>
2024-04-14 17:11:17 +0200

    AddReservation update

--- src/GUI/Controller/AddReservationViewController.java
+++ src/GUI/Controller/AddReservationViewController.java
@@ -1,5 +1,6 @@
 package GUI.Controller;
 
+import GUI.Model.TicketModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,27 +8,46 @@ import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class AddReservationViewController implements Initializable {
 
-    public Label labelEvent;
     @FXML
-    private MenuButton ticketTypeDropDown;
+    private Label lblQuantity;
+
+    @FXML
+    private Label lblTicketType;
+
+    @FXML
+    private Label lblEventTitle;
+
 
     @FXML
     private TextField txtFieldCostumerEmail;
 
-    @FXML
-    private TextField txtFieldQuantity;
 
 
+    private String eventTitle;
+
+    private TicketModel ticketModel;
+
+    public AddReservationViewController() throws SQLException, IOException {
+        ticketModel = new TicketModel();
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    public void setEventData(String eventTitle, String ticketTypeTitle) {
+        lblEventTitle.setText(eventTitle);
+        lblTicketType.setText(ticketTypeTitle);
+    }
+
     @FXML
     public void onClickCancel(ActionEvent event) {
 
@@ -35,13 +55,9 @@ public class AddReservationViewController implements Initializable {
 
     @FXML
     public void onClickSave(ActionEvent event) {
+        String email = txtFieldCostumerEmail.getText(); // Get customer email from input field
+        String eventTitle = lblEventTitle.getText(); // Get event title from label
+        String ticketTypeTitle = lblTicketType.getText();
 
     }
-
-    @FXML
-    public void onClickTicketTypeDropDown(ActionEvent event) {
-
-    }
-
-
 }
